78a9ec529750551bf6e9a762433e9da95515c7b3
Jayesh Kaza <jayesh.kaza@zemosolabs.com>
2019-08-21 14:29:56 +0530

    minor modifications done

--- src/main/java/com/springboot/Employee/Rest/API/Service/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/Employee/Rest/API/Service/EmployeeServiceImpl.java
@@ -5,7 +5,6 @@ import com.springboot.Employee.Rest.API.Exceptions.EmployeeNotFoundException;
 import com.springboot.Employee.Rest.API.SpringDataJPA.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
