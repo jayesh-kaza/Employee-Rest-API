9601c796773641edd19ba09cd9e753e9dbd377c8
Jayesh Kaza <jayesh.kaza@zemosolabs.com>
2019-08-21 15:19:34 +0530

    spring data rest done

--- src/main/java/com/springboot/Employee/Rest/API/SpringDataJPA/EmployeeRepository.java
+++ src/main/java/com/springboot/Employee/Rest/API/SpringDataJPA/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.springboot.Employee.Rest.API.SpringDataJPA;
 
 import com.springboot.Employee.Rest.API.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-                                                        //entity type, primary key
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+// to put custom mapping
+@RepositoryRestResource(path="members")                  //entity type, primary key
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 // no need to write code.
 }
